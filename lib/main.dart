bfde990deae122d286be99198929ded95fe89733
Chinz233 <seanchin233@gmail.com>
2026-06-20 01:29:16 +0800

    Changed deprecated syntax
    For .env files and Supabase initialisation.

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ Future<void> main() async {
   await dotenv.load(fileName: '.env');
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL'] ?? '',
-    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+    publishableKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
   );
   runApp(const ProviderScope(child: PlanNUSApp()));
 }
